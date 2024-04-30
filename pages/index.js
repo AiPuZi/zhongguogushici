3d3b60ddc337dfcfda6cbeceace2d6812d85bbb1
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 21:29:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,19 @@ async function getPoetryData(category, page, perPage) {
 
 // 使用 getStaticProps 来预渲染页面
 export async function getStaticProps() {
-  const baseUrl = process.env.API_BASE_URL; // 使用环境变量中定义的API基础URL
+  const baseUrl = process.env.API_BASE_URL;
   const response = await fetch(`${baseUrl}/api/search?category=quantangshi&page=0&perPage=9`);
   const poetryData = await response.json();
   return {
     props: {
       initialPoetryData: poetryData.map(poem => ({
         ...poem,
+        author: poem.author, 
         content: Array.isArray(poem.content) ? poem.content : [],
         comment: Array.isArray(poem.comment) ? poem.comment : []
       })),
     },
-    revalidate: 10, // ISR: 页面内容更新后，每10秒重新生成一次
+    revalidate: 10,
   };
 }
 
