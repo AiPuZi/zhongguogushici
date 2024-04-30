e77de73ddc8400823e832626f8ac87c1c268901c
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 14:14:45 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,11 +1,6 @@
 export default function Poem({ title, author, paragraphs }) {
-  // 检查每个段落是否包含换行符，并将它们替换为 <br> 标签
-  const processedParagraphs = paragraphs.map(paragraph =>
-    paragraph.includes('<br>') ? paragraph : paragraph.replace(/\n/g, '<br>')
-  );
-
-  // 将处理后的段落数组转换成一个字符串，段落之间用 <br><br> 分隔
-  const combinedParagraphs = processedParagraphs.join('<br><br>');
+  // 将段落数组转换成一个字符串，段落之间用 <br><br> 分隔
+  const combinedParagraphs = paragraphs.join('<br><br>');
 
   return (
     <div>
