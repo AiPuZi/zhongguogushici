0b72eb4867dac4d94706a2567edbe0b36ee203bf
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 13:52:08 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -4,7 +4,12 @@ export default function Poem({ title, author, paragraphs }) {
       {title && <h3>{title}</h3>}
       {author && <p>{author}</p>}
       {paragraphs.map((paragraph, index) => (
-        <p key={index}>{paragraph}</p>
+        // 使用 split 方法将每个段落的字符串按 <br> 分割，然后遍历数组渲染每一行
+        <div key={index}>
+          {paragraph.split('<br>').map((line, lineIndex) => (
+            <p key={lineIndex}>{line}</p>
+          ))}
+        </div>
       ))}
     </div>
   );
