afeeae39f838963b9bb0f3164b1a5afaff88e493
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 23:04:04 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,13 +1,10 @@
-export default function Poem({ title, author, section, chapter, content }) {
-  // 将内容数组转换成字符串，每段之间用 <br><br> 分隔
-  const combinedContent = Array.isArray(content) ? content.join('<br><br>') : '';
+export default function Poem({ title, author, content }) {
+  const combinedContent = content.join('<br><br>');
 
   return (
     <div>
       <h3>{title}</h3>
       <h4>{author}</h4>
-      <h5>{section}</h5>
-      <h5>{chapter}</h5>
       <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
     </div>
   );
