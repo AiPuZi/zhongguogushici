afcbfd4fe46bd847326ab6fa4a760de67a18f85a
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 20:26:02 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -7,10 +7,15 @@ export default function Poem({ title, chapter, section, content, comment }) {
 
   return (
     <div>
+      {/* 只有当 title 存在时，才显示标题 */}
       {title && <h3>{title}</h3>}
+      {/* 只有当 chapter 存在时，才显示章节 */}
       {chapter && <h4>{chapter}</h4>}
+      {/* 只有当 section 存在时，才显示节 */}
       {section && <h5>{section}</h5>}
+      {/* 显示内容 */}
       <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
+      {/* 如果存在评论，显示评论 */}
       {combinedComment && (
         <div className="comments" dangerouslySetInnerHTML={{ __html: combinedComment }} />
       )}
