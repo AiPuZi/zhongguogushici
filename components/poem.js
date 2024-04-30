a8708109a2ba6f8b9878a824a520e3fd918da6a2
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 20:23:36 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,15 +1,19 @@
-import React from 'react';
+export default function Poem({ title, chapter, section, content, comment }) {
+  // 将内容数组转换成字符串，每段之间用 <br><br> 分隔
+  const combinedContent = content.join('<br><br>');
 
-export default function Poem({ title, author, paragraphs }) {
-  const safeParagraphs = Array.isArray(paragraphs) ? paragraphs : [];
-  const hasContent = safeParagraphs.some(paragraph => paragraph.trim() !== '');
-  const combinedParagraphs = hasContent ? safeParagraphs.join('<br><br>') : '内容不可用';
+  // 如果存在评论，则将其也转换成字符串
+  const combinedComment = comment ? comment.join('<br><br>') : null;
 
   return (
     <div>
       {title && <h3>{title}</h3>}
-      {author && <p>{author}</p>}
-      <div dangerouslySetInnerHTML={{ __html: combinedParagraphs }} />
+      {chapter && <h4>{chapter}</h4>}
+      {section && <h5>{section}</h5>}
+      <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
+      {combinedComment && (
+        <div className="comments" dangerouslySetInnerHTML={{ __html: combinedComment }} />
+      )}
     </div>
   );
 }
