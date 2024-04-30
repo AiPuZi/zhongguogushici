4db6a5497c458b3bde544353a54f51d91c3da466
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-30 17:18:47 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,13 +1,14 @@
-  const hasContent = safeParagraphs.some(paragraph => paragraph.trim() !== '');
+import React from 'react';
 
-  // 将段落数组转换成一个字符串，段落之间用 <br><br> 分隔
+export default function Poem({ title, author, paragraphs }) {
+  const safeParagraphs = Array.isArray(paragraphs) ? paragraphs : [];
+  const hasContent = safeParagraphs.some(paragraph => paragraph.trim() !== '');
   const combinedParagraphs = hasContent ? safeParagraphs.join('<br><br>') : '内容不可用';
 
   return (
     <div>
       {title && <h3>{title}</h3>}
       {author && <p>{author}</p>}
-      {/* 使用 dangerouslySetInnerHTML 渲染包含 <br> 的段落字符串 */}
       <div dangerouslySetInnerHTML={{ __html: combinedParagraphs }} />
     </div>
   );
