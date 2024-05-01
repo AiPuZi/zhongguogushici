64927be0300baae37338c8debe4111eaac31b3d9
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-01 10:33:18 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -143,7 +143,6 @@ export default function Home({ initialPoetryData }) {
         section={poem.section}
         chapter={poem.chapter}
         content={poem.content}
-        chapter={poem.chapter}
       />
     </div>
   ))}
