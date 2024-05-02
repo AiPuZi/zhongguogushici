37568a8ab19aecb789cc1350cedadbdf47a74fd2
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-03 00:39:06 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -18,12 +18,12 @@ export default function Poem({ title, author, content, chapter, section, comment
   }
 
   // 否则，返回包含章节、标题、作者、内容和评论的完整结构
- return (
+  return (
     <div>
-      {section && <h4>{section}</h4>}
-      {chapter && <h5>{chapter}</h5>}
-      {title && <h3>{title}</h3>}
-      {author && <h4>{author}</h4>}
+      {section && <h4>{section}</h4>} // 显示 section 如果存在
+      {chapter && <h5>{chapter}</h5>} // 显示 chapter 如果存在
+      <h3>{title}</h3> // 总是显示 title，因为现在我们知道它总是存在
+      {author && <h4>{author}</h4>} // 显示 author 如果存在
       <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
       <div className="comments">
         {commentElements}
