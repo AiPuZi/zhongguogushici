6b7a1025c45a3b9bad44ad4270584f0fcee95dad
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-02 23:38:56 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,16 +1,32 @@
 export default function Poem({ title, author, content, chapter, comments }) {
+  // 将内容数组转换为字符串，并用 <br><br> 分隔
   const combinedContent = content.join('<br><br>');
+
+  // 生成评论元素
   const commentElements = comments.map((comment, index) => (
     <p key={index}>{comment}</p>
   ));
 
+  // 如果标题和作者都不存在，直接返回内容和评论
+  if (!title && !author) {
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
+        <div className="comments">
+          {commentElements}
+        </div>
+      </div>
+    );
+  }
+
+  // 否则，返回包含章节、标题、作者、内容和评论的完整结构
   return (
     <div>
-     {chapter && <h5>{chapter}</h5>}
+      {chapter && <h5>{chapter}</h5>}
       <h3>{title}</h3>
       <h4>{author}</h4>
       <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
-  <div className="comments">
+      <div className="comments">
         {commentElements}
       </div>
     </div>
