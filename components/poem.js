47096b94997baf849aea3207d8f356026ca65fcb
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-03 22:36:24 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,21 +1,18 @@
-export default function Poem({ title, author, content, paragraphs, chapter, section, comments = [], rhythmic }) {
+export default function Poem({ title, author, content, chapter, section, comments = [], rhythmic }) {
   const combinedContent = Array.isArray(content) ? content.join('<br><br>') : content;
   const subtitle = chapter || section; // 定义副标题
-  
-  // 处理评论元素，同时展示 surname 和 place
   const commentElements = comments.map((comment, index) => (
-    <p key={index}>{comment.surname} - {comment.place}</p>
+    <p key={index}>{comment}</p>
   ));
 
   return (
     <div>
       {subtitle && <h4>{subtitle}</h4>}
+      {/* 如果有 title，就展示 title，否则如果有 rhythmic，就展示 rhythmic */}
       {title ? <h3>{title}</h3> : (rhythmic && <h3>{rhythmic}</h3>)}
-      {paragraphs && paragraphs.map((para, index) => <p key={index}>{para}</p>)}
       {author && <h4>{author}</h4>}
       <div dangerouslySetInnerHTML={{ __html: combinedContent }} />
       <div className="comments">
-        {/* 直接使用 commentElements 展示 surname 和 place */}
         {commentElements}
       </div>
     </div>
