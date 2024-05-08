5dbaa65a4454877cf1bcb2c758703a8b6f6301aa
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-08 14:48:46 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -4,21 +4,23 @@ export default function Poem({ title, author, content, chapter, section, comment
     <p key={index}>{comment}</p>
   ));
 
-// 如果 content 是一个数组，则将其连接为一个字符串。（搜索结果中只显示标题和作者不能显示内容，就加了这段代码）
+  let contentElements = null;
   if (Array.isArray(content)) {
-    content = content.join('\n');
+    // 如果内容是字符串数组，则将每行内容渲染为一个段落
+    contentElements = content.map((line, index) => <p key={index} style={{ marginBottom: '16px' }}>{line}</p>);
+  } else {
+    // 如果内容是字符串，则直接渲染为一个段落
+    contentElements = <p>{content}</p>;
   }
-  
+
   return (
     <div>
       {subtitle && <h4>{subtitle}</h4>}
       {title ? <h3>{title}</h3> : (rhythmic && <h3>{rhythmic}</h3>)}
       {author && <h4>{author}</h4>}
-      {/* 直接渲染content数组的每个元素为一个段落 */}
+      {/* 渲染诗词内容 */}
       <div>
-        {Array.isArray(content) ? content.map((paragraph, index) => (
-          <p key={index} style={{ marginBottom: '16px' }}>{paragraph}</p>
-        )) : <p>{content}</p>}
+        {contentElements}
       </div>
       <div className="comments">
         {commentElements}
