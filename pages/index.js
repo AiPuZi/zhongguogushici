14e70dafec5fc2ec42c3685bb01fc7ab56b6f768
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-08 20:25:03 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -107,33 +107,31 @@ function Home({ initialPoetryData }) {
   };
 
   const handleNextPage = async () => {
-  if (isLoadingMore) return;
+  if (currentPage + 1 < pageList.length) {
+    setCurrentPage(currentPage + 1);
+  } else {
+    const nextPage = currentPage + 1;
+    const data = await fetchData(currentCategory, nextPage, poemsPerPage, searchKeyword);
 
-  setIsLoadingMore(true);
+    setPageList((prevList) => [
+      ...prevList.slice(-maxCachedPages + 1),
+      data,
+    ]);
 
-  const nextPage = currentPage + 1;
-  const totalPages = Math.ceil(poetryData.length / poemsPerPage);
-
-  // 首先检查是否有预加载的下一页数据
-  if (nextPage < pageList.length) {
     setCurrentPage(nextPage);
-    setIsLoadingMore(false);
-    return;
   }
-
-  // 如果没有预加载数据，则请求新的数据
-  if (nextPage <= totalPages) {
-    try {
-      const data = await fetchData(currentCategory, nextPage, poemsPerPage, searchKeyword);
-      setPageList([...pageList, data]); // 添加新数据到缓存列表末尾
-      setCurrentPage(nextPage);
-    } catch (error) {
-      console.error("Error fetching next page:", error);
-    } finally {
-      setIsLoadingMore(false);
-    }
-  }
+  
+  // 重新加载数据
+  await fetchDataAndSetPoetryData();
 };
+
+const handlePreviousPage = async () => {
+  setCurrentPage(Math.max(currentPage - 1, 0));
+  
+  // 重新加载数据
+  await fetchDataAndSetPoetryData();
+};
+
 
   const handlePreviousPage = () => {
     setCurrentPage(Math.max(currentPage - 1, 0));
