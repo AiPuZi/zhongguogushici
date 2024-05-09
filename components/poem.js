8a0ee0284f5d43e8d650c1cbbd9b9c453f7f67df
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-09 16:10:32 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,8 +1,4 @@
 export default function Poem({ title, author, content, chapter, section, comments = [], rhythmic }) {
-  // 确保 chapter 和 paragraphs 属性存在，即使它们未在组件中使用
-  chapter = chapter || "";
-  const paragraphs = Array.isArray(content) ? content : [content];
-  
   const subtitle = chapter || section; // 定义副标题
   const commentElements = comments.map((comment, index) => (
     <p key={index}>{comment}</p>
@@ -11,12 +7,14 @@ export default function Poem({ title, author, content, chapter, section, comment
   return (
     <div>
       {subtitle && <h4>{subtitle}</h4>}
-      {title && <h3>{title}</h3>}
+      {title ? <h3>{title}</h3> : (rhythmic && <h3>{rhythmic}</h3>)}
       {author && <h4>{author}</h4>}
-      {/* 渲染段落 */}
-      {paragraphs.map((paragraph, index) => (
-        <p key={index} style={{ marginBottom: '16px' }}>{paragraph}</p>
-      ))}
+      {/* 直接渲染content数组的每个元素为一个段落 */}
+      <div>
+        {Array.isArray(content) ? content.map((paragraph, index) => (
+          <p key={index} style={{ marginBottom: '16px' }}>{paragraph}</p>
+        )) : <p>{content}</p>}
+      </div>
       <div className="comments">
         {commentElements}
       </div>
