2ed1310998d289ad31a9ff968699f6de08cf8311
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-09 17:31:01 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -108,16 +108,8 @@ function Home({ initialPoetryData }) {
       </header>
 
       <nav className="poetry-navigation">
-       <Link href="/quantangshi">
-       <a onClick={(e) => handleCategoryChange('quantangshi')}>
-       {'全唐诗'}
-        </a>
-        </Link>
-        <Link href="/tangshisanbaishou">
-        <a onClick={(e) => handleCategoryChange('tangshisanbaishou')}>
-        {'唐三百'}
-        </a>
-        </Link>
+        <a href="/quantangshi" onClick={(e) => handleCategoryChange('quantangshi', e)}>全唐诗</a>
+        <a href="/tangshisanbaishou" onClick={(e) => handleCategoryChange('tanshisanbaishou', e)}>唐三百</a>
         <a href="/shuimotangshi" onClick={(e) => handleCategoryChange('shuimotangshi', e)}>水墨唐诗</a>
         <a href="/yudingquantangshi" onClick={(e) => handleCategoryChange('yudingquantangshi', e)}>御定全唐诗</a>
         <a href="/quansongci" onClick={(e) => handleCategoryChange('quansongci', e)}>全宋词</a>
