b5961ecb5809121fad0b7a61ae741e2cfb8c290c
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-11 14:23:55 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,5 +1,5 @@
 export default function Poem({ title, author, content, chapter, source, rhythmic, paragraphs, comments = [], abstract }) {
-  const subtitle = chapter || source || rhythmic || title; // 根据优先级选择副标题
+  const subtitle = chapter || source || rhythmic; // 根据优先级选择副标题
   const commentElements = comments.map((comment, index) => (
     <p key={index}>{comment}</p>
   ));
@@ -10,8 +10,8 @@ export default function Poem({ title, author, content, chapter, source, rhythmic
         <p key={index} style={{ fontStyle: 'italic' }}>{paragraph}</p>
       ))}
       {subtitle && <h4>{subtitle}</h4>}
-      {title && <h3>{title}</h3>}
-      {(author && !paragraphs) && <h4>{author}</h4>}
+      {(title && !paragraphs) && <h3>{title}</h3>}
+      {author && <h4>{author}</h4>}
       {source && <h4>{source}</h4>}
       {rhythmic && <h3>{rhythmic}</h3>}
       {content && (
