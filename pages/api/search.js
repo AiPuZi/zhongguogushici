4abac192abc8060742d69a5b33e14873cbcf6746
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-11 11:45:51 +0800

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -4,56 +4,61 @@ import path from 'path';
 export default async function handler(req, res) {
   const { query } = req.query;
 
-  if (query) {
-    const poems = await searchPoems(query);
-    res.status(200).json(poems);
-    return; // 结束响应
-  } else {
+  if (!query) {
     res.status(400).json({ error: 'Missing query parameter' });
     return; // 结束响应
   }
+
+  try {
+    const poems = await searchPoems(query);
+    res.status(200).json(poems);
+  } catch (error) {
+    console.error('Error searching for poems:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
-const searchPoems = async (keyword) => {
+async function searchPoems(keyword) {
   const categories = await readdir(path.join(process.cwd(), 'public'));
 
+  // 并行处理搜索操作
+  const matchingPoems = await Promise.all(categories.map(category => searchCategory(category, keyword)));
+
+  return matchingPoems.flat();
+}
+
+async function searchCategory(category, keyword) {
+  const categoryDirPath = path.join(process.cwd(), 'public', category);
+  const categoryStat = await stat(categoryDirPath);
+
+  if (!categoryStat.isDirectory()) {
+    return []; // 如果不是目录，则返回空数组
+  }
+
+  const files = await readdir(categoryDirPath);
+  const validFiles = files.filter(file => file.endsWith('.json'));
   const poems = [];
 
-  for (const category of categories) {
-    const categoryDirPath = path.join(process.cwd(), 'public', category);
-     // 使用 stat 检查是否为目录
-    const categoryStat = await stat(categoryDirPath);
-    if (!categoryStat.isDirectory()) {
-      continue; // 如果不是目录，则跳过当前循环
+  for (const file of validFiles) {
+    const filePath = path.join(categoryDirPath, file);
+    const fileContent = await readFile(filePath, 'utf8');
+    const jsonContent = JSON.parse(fileContent);
+
+    if (!Array.isArray(jsonContent) || jsonContent.length === 0) {
+      console.error(`File ${filePath} does not contain an array or is empty.`);
+      continue;
     }
-    const files = await readdir(categoryDirPath);
-    const validFiles = files.filter(file => file.endsWith('.json'));
 
-    for (const file of validFiles) {
-      const filePath = path.join(categoryDirPath, file);
-      const fileContent = await readFile(filePath, 'utf8');
-      const jsonContent = JSON.parse(fileContent);
-
-      if (!Array.isArray(jsonContent) || jsonContent.length === 0) {
-        console.error(`File ${filePath} does not contain an array or is empty.`);
-        continue;
-      }
-
-      const batchSize = 1000; // 每批处理的诗词数目
-      for (let i = 0; i < jsonContent.length; i += batchSize) {
-        const batch = jsonContent.slice(i, i + batchSize);
-        for (const item of batch) {
-          if (poemsPushIfMatched(item, keyword)) {
-            // 如果找到匹配项，直接推送到结果数组中，无需break
-            poems.push(item);
-          }
-        }
+    for (const item of jsonContent) {
+      if (poemsPushIfMatched(item, keyword)) {
+        // 如果找到匹配项，直接推送到结果数组中
+        poems.push(item);
       }
     }
   }
 
   return poems;
-};
+}
 
 function poemsPushIfMatched(item, keyword) {
   for (const key in item) {
@@ -61,11 +66,11 @@ function poemsPushIfMatched(item, keyword) {
       const value = item[key];
 
       if (isStringMatch(value, keyword) || isArrayMatch(value, keyword)) {
-        return item; // 返回整个匹配的诗词对象
+        return true; // 找到匹配项，返回true
       }
     }
   }
-  return null; // 没有找到匹配项，返回null
+  return false; // 没有找到匹配项，返回false
 }
 
 function isStringMatch(value, keyword) {
