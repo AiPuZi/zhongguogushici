c398f0787676885036c9a95f17a32225214a6dc9
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-11 14:10:26 +0800

    Update poem.js

--- components/poem.js
+++ components/poem.js
@@ -1,20 +1,40 @@
-export default function Poem({ title, author, content, chapter, section, comments = [], rhythmic }) {
-  const subtitle = chapter && section ? `${chapter} - ${section}` : chapter || section;// 定义副标题
+export default function Poem({ title, author, content, chapter, source, rhythmic, paragraphs, comments = [], abstract }) {
+  const subtitle = chapter || source || rhythmic || author || title; // 根据优先级选择副标题
   const commentElements = comments.map((comment, index) => (
     <p key={index}>{comment}</p>
   ));
 
   return (
     <div>
+      {abstract && abstract.map((paragraph, index) => (
+        <p key={index} style={{ fontStyle: 'italic' }}>{paragraph}</p>
+      ))}
       {subtitle && <h4>{subtitle}</h4>}
-      {title ? <h3>{title}</h3> : (rhythmic && <h3>{rhythmic}</h3>)}
+      {title && <h3>{title}</h3>}
       {author && <h4>{author}</h4>}
-      {/* 直接渲染content数组的每个元素为一个段落 */}
-      <div>
-        {Array.isArray(content) ? content.map((paragraph, index) => (
-          <p key={index} style={{ marginBottom: '16px' }}>{paragraph}</p>
-        )) : <p>{content}</p>}
-      </div>
+      {source && <h4>{source}</h4>}
+      {rhythmic && <h3>{rhythmic}</h3>}
+      {content && (
+        <div>
+          {Array.isArray(content) ? content.map((paragraph, index) => (
+            <p key={index} style={{ marginBottom: '16px' }}>{paragraph}</p>
+          )) : <p>{content}</p>}
+        </div>
+      )}
+      {paragraphs && paragraphs.map((paragraph, index) => (
+        <div key={index}>
+          {paragraph.chapter && <h4>{paragraph.chapter}</h4>}
+          {paragraph.source && <h4>{paragraph.source}</h4>}
+          {paragraph.author && <h4>{paragraph.author}</h4>}
+          {paragraph.paragraphs && (
+            <div>
+              {paragraph.paragraphs.map((p, idx) => (
+                <p key={idx} style={{ marginBottom: '16px' }}>{p}</p>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
       <div className="comments">
         {commentElements}
       </div>
