117563075452678d0ed5c5e16e9c715da7cbd7a6
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-14 12:58:28 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Poem from '../components/poem';
 import { useRouter } from 'next/router';
-import * as OpenCC from 'opencc-js/core'; // 导入opencc-js核心
-import * as Locale from 'opencc-js/preset'; // 导入opencc-js预设
+import Link from 'next/link';
 
 async function fetchData(category, page, perPage, keyword) {
   let url = `/api/poems?category=${category}&page=${page}&perPage=${perPage}`;
@@ -41,17 +40,9 @@ export async function getStaticProps() {
     content: Array.isArray(item.paragraphs) ? item.paragraphs : item.content || item.para || [],
   })) : [];
 
-  const converter = OpenCC.ConverterFactory(Locale.from.hk, Locale.to.cn);
-
-  // 将页面中的所有繁体字转换为简体字
-  const simplifiedPoetryData = poetryData.map(item => {
-    const simplifiedContent = item.content.map(paragraph => converter(paragraph)).join('\n');
-    return { ...item, content: simplifiedContent };
-  });
-
   return {
     props: {
-      initialPoetryData: simplifiedPoetryData,
+      initialPoetryData: poetryData,
     },
     revalidate: 10,
   };
@@ -66,86 +57,67 @@ function Home({ initialPoetryData }) {
   const [currentPage, setCurrentPage] = useState(0);
   const poemsPerPage = 9;
 
-useEffect(() => {
-  let cancel = false;
-  const fetchDataAndSetPoetryData = async () => {
-    const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
-    const data = await fetchData(currentCategory, currentPage, poemsPerPage, keyword);
-    if (!cancel) {
-      const converter = OpenCC.ConverterFactory(Locale.from.hk, Locale.to.cn);
-      const simplifiedData = data.map(item => ({
-        ...item,
-        content: item.content.map(paragraph => converter(paragraph).split('\n')),
-      }));
-      setPoetryData(simplifiedData);
-      if (currentPage === 0) {
-        preFetchNextPage(currentCategory, currentPage, poemsPerPage, keyword, setNextPageData);
+  useEffect(() => {
+    let cancel = false;
+    const fetchDataAndSetPoetryData = async () => {
+      const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
+      const data = await fetchData(currentCategory, currentPage, poemsPerPage, keyword);
+      if (!cancel) {
+        setPoetryData(data);
+        if (currentPage === 0) {
+          preFetchNextPage(currentCategory, currentPage, poemsPerPage, keyword, setNextPageData); // Pre-fetch data for next page
+        }
       }
+    };
+
+    if (currentCategory !== '') {
+      fetchDataAndSetPoetryData();
     }
-  };
 
-  if (initialPoetryData && initialPoetryData.length > 0 && currentPage === 0) {
-    // 这里添加 currentPage === 0 的判断条件，以确保只在初次加载首页时设置诗词数据
-    setPoetryData(initialPoetryData);
-  } else {
-    fetchDataAndSetPoetryData();
-  }
-
-  return () => {
-    cancel = true;
-  };
-}, [currentCategory, currentPage, poemsPerPage, router.query.query, initialPoetryData]);
+    return () => {
+      cancel = true;
+    };
+  }, [currentCategory, currentPage, poemsPerPage, router.query.query]);
 
   const handleCategoryChange = async (category, event) => {
-  event.preventDefault();
-  setCurrentCategory(category);
-  setCurrentPage(0); // 重置当前页数为第一页
-};
+    event.preventDefault();
+    setCurrentCategory(category);
+    setCurrentPage(0);
+    const data = await fetchData(category, 0, poemsPerPage, '');
+    setPoetryData(data);
+    preFetchNextPage(category, 0, poemsPerPage, '', setNextPageData); // Pre-fetch data for next page
+  };
 
   const handleSearch = async (event) => {
-    event.preventDefault();
-    const data = await fetchData(currentCategory, 0, poemsPerPage, searchKeyword);
-    const converter = OpenCC.ConverterFactory(Locale.from.hk, Locale.to.cn);
-    const simplifiedData = data.map(item => ({
-      ...item,
-      content: item.content.map(paragraph => converter(paragraph)).join('\n'), // 进行繁简转换
-    }));
-    setPoetryData(simplifiedData);
-    if (data.length < poemsPerPage) {
-      // 如果搜索结果不足一页，则禁用下一页按钮
-      setNextPageData([]);
-    } else {
-      preFetchNextPage(currentCategory, 0, poemsPerPage, searchKeyword, setNextPageData); // Pre-fetch data for next page
-    }
-  };
+  event.preventDefault();
+  const data = await fetchData(currentCategory, 0, poemsPerPage, searchKeyword);
+  setPoetryData(data);
+  if (data.length < poemsPerPage) {
+    // 如果搜索结果不足一页，则禁用下一页按钮
+    setNextPageData([]);
+  } else {
+    preFetchNextPage(currentCategory, 0, poemsPerPage, searchKeyword, setNextPageData); // Pre-fetch data for next page
+  }
+};
 
   const goToNextPage = async () => {
   if (nextPageData.length > 0) {
-    setCurrentPage(prevPage => {
-      const nextPage = prevPage + 1;
-      const converter = OpenCC.ConverterFactory(Locale.from.hk, Locale.to.cn);
-      const simplifiedData = nextPageData.map(item => ({
-        ...item,
-        content: item.content.map(paragraph => converter(paragraph).split('\n')),
-      }));
-      setPoetryData(simplifiedData);
-      setNextPageData([]);
-      const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
-      router.push({
-        pathname: router.pathname,
-        query: { ...router.query, page: nextPage },
-      });
-      return nextPage;
-    });
-  } else {
-    console.log("没有下一页数据可加载");
+    // 增加当前页的页数
+    setCurrentPage((prevPage) => prevPage + 1);
+    // 更新页面数据为预取的数据
+    setPoetryData(nextPageData);
+    // 清空nextPageData状态，以便下一次的预取操作
+    setNextPageData([]);
+    // 预取下一页的数据
+    const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
+    await preFetchNextPage(currentCategory, currentPage + 1, poemsPerPage, keyword, setNextPageData);
   }
 };
 
   const goToPrevPage = () => {
     setCurrentPage(prevPage => (prevPage > 0 ? prevPage - 1 : 0));
   };
-
+  
   return (
     <>
       <Head>
@@ -171,25 +143,25 @@ useEffect(() => {
       </header>
 
       <nav className="poetry-navigation">
-        <a onClick={(e) => handleCategoryChange('quantangshi', e)}>全唐诗</a>
-        <a onClick={(e) => handleCategoryChange('tangshisanbaishou', e)}>唐三百</a> 
-        <a onClick={(e) => handleCategoryChange('shuimotangshi', e)}>水墨唐诗</a>
-        <a onClick={(e) => handleCategoryChange('yudingquantangshi', e)}>御定全唐诗</a>
-        <a onClick={(e) => handleCategoryChange('quansongci', e)}>全宋词</a>
-        <a onClick={(e) => handleCategoryChange('songcisanbaishou', e)}>宋三百</a>
-        <a onClick={(e) => handleCategoryChange('yuanqu', e)}>元曲</a>
-        <a onClick={(e) => handleCategoryChange('huajianji', e)}>花间集</a>
-        <a onClick={(e) => handleCategoryChange('nantangerzhuci', e)}>南唐二主词</a>
-        <a onClick={(e) => handleCategoryChange('shijing', e)}>诗经</a>
-        <a onClick={(e) => handleCategoryChange('chuci', e)}>楚辞</a>
-        <a onClick={(e) => handleCategoryChange('lunyu', e)}>论语</a>
-        <a onClick={(e) => handleCategoryChange('mengxue', e)}>蒙学</a>
-        <a onClick={(e) => handleCategoryChange('nalanxingde', e)}>纳兰性德</a>
-        <a onClick={(e) => handleCategoryChange('youmengying', e)}>幽梦影</a>
-        <a onClick={(e) => handleCategoryChange('caocaoshiji', e)}>曹操诗集</a>
+        <a href="/quantangshi" onClick={(e) => handleCategoryChange('quantangshi', e)}>全唐诗</a>
+        <a href="/tangshisanbaishou" onClick={(e) => handleCategoryChange('tangshisanbaishou', e)}>唐三百</a> 
+        <a href="/shuimotangshi" onClick={(e) => handleCategoryChange('shuimotangshi', e)}>水墨唐诗</a>
+        <a href="/yudingquantangshi" onClick={(e) => handleCategoryChange('yudingquantangshi', e)}>御定全唐诗</a>
+        <a href="/quansongci" onClick={(e) => handleCategoryChange('quansongci', e)}>全宋词</a>
+        <a href="/songcisanbaishou" onClick={(e) => handleCategoryChange('songcisanbaishou', e)}>宋三百</a>
+        <a href="/yuanqu" onClick={(e) => handleCategoryChange('yuanqu', e)}>元曲</a>
+        <a href="/huajianji" onClick={(e) => handleCategoryChange('huajianji', e)}>花间集</a>
+        <a href="/nantangerzhuci" onClick={(e) => handleCategoryChange('nantangerzhuci', e)}>南唐二主词</a>
+        <a href="/shijing" onClick={(e) => handleCategoryChange('shijing', e)}>诗经</a>
+        <a href="/chuci" onClick={(e) => handleCategoryChange('chuci', e)}>楚辞</a>
+        <a href="/lunyu" onClick={(e) => handleCategoryChange('lunyu', e)}>论语</a>
+        <a href="/mengxue" onClick={(e) => handleCategoryChange('mengxue', e)}>蒙学</a>
+        <a href="/nalanxingde" onClick={(e) => handleCategoryChange('nalanxingde', e)}>纳兰性德</a>
+        <a href="/youmengying" onClick={(e) => handleCategoryChange('youmengying', e)}>幽梦影</a>
+        <a href="/caocaoshiji" onClick={(e) => handleCategoryChange('caocaoshiji', e)}>曹操诗集</a>
       </nav>
       
-      <main id="poetry-content">
+<main id="poetry-content">
         {Array.isArray(poetryData) && poetryData.map((poem, index) => (
           <div key={index} className="poem">
             <Poem
