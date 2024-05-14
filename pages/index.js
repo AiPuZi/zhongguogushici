902a2927af17a99deee56e77050bb3e6d66502d6
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-14 16:34:50 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -183,9 +183,9 @@ function Home({ initialPoetryData }) {
         <button onClick={goToNextPage} disabled={nextPageData.length === 0}>下一页</button>
       </div>
 
-      <div className="attribution">
-        本站在使用上还存在一些小问题，详情请至留言板查看或反馈。    
-        <br /><a href="https://www.winglok.com" target="_blank">留言板</a>
+      <div className="attribution">    
+        <br />网站使用问题汇总与反馈：<a href="https://www.winglok.com" target="_blank">留言板</a>
+        <br />公众号：每天一诗
       </div>
       
       <footer>
