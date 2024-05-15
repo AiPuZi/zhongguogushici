568459521a9398c80c444344faa39d5a47b71d09
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-15 12:20:42 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -102,15 +102,18 @@ function Home({ initialPoetryData }) {
 
   const goToNextPage = async () => {
   if (nextPageData.length > 0) {
-    // 增加当前页的页数
-    setCurrentPage((prevPage) => prevPage + 1);
-    // 更新页面数据为预取的数据
-    setPoetryData(nextPageData);
-    // 清空nextPageData状态，以便下一次的预取操作
-    setNextPageData([]);
-    // 预取下一页的数据
-    const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
-    await preFetchNextPage(currentCategory, currentPage + 1, poemsPerPage, keyword, setNextPageData);
+    // 增加当前页的页数，并确保在状态更新后执行预取下一页的数据
+    setCurrentPage((prevPage) => {
+      const newPage = prevPage + 1;
+      // 更新页面数据为预取的数据
+      setPoetryData(nextPageData);
+      // 清空nextPageData状态，以便下一次的预取操作
+      setNextPageData([]);
+      // 预取下一页的数据
+      const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
+      preFetchNextPage(currentCategory, newPage, poemsPerPage, keyword, setNextPageData);
+      return newPage;
+    });
   }
 };
 
