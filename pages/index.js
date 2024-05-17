f6d69e820ff42b070540bc4aa5d138f9cea3b241
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-05-17 14:27:46 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -89,33 +89,34 @@ function Home({ initialPoetryData }) {
   };
 
   const handleSearch = async (event) => {
-  event.preventDefault();
-  const data = await fetchData(currentCategory, 0, poemsPerPage, searchKeyword);
-  setPoetryData(data);
-  if (data.length < poemsPerPage) {
-    // 如果搜索结果不足一页，则禁用下一页按钮
-    setNextPageData([]);
-  } else {
-    preFetchNextPage(currentCategory, 0, poemsPerPage, searchKeyword, setNextPageData); // Pre-fetch data for next page
-  }
-};
+    event.preventDefault();
+    const data = await fetchData(currentCategory, 0, poemsPerPage, searchKeyword);
+    setPoetryData(data);
+    if (data.length < poemsPerPage) {
+      // 如果搜索结果不足一页，则禁用下一页按钮
+      setNextPageData([]);
+    } else {
+      preFetchNextPage(currentCategory, 0, poemsPerPage, searchKeyword, setNextPageData); // Pre-fetch data for next page
+    }
+  };
 
   const goToNextPage = async () => {
-  if (nextPageData.length > 0) {
-    // 增加当前页的页数
-    setCurrentPage((prevPage) => prevPage + 1);
-    // 更新页面数据为预取的数据
+    // 先增加当前页的页数
+    const newPage = currentPage + 1;
+    setCurrentPage(newPage);
+
+    // 等待 currentPage 更新完成
     setPoetryData(nextPageData);
-    // 清空nextPageData状态，以便下一次的预取操作
     setNextPageData([]);
+
     // 预取下一页的数据
     const keyword = router.query.query ? decodeURIComponent(router.query.query) : '';
-    await preFetchNextPage(currentCategory, currentPage + 1, poemsPerPage, keyword, setNextPageData);
-  }
-};
+    await preFetchNextPage(currentCategory, newPage, poemsPerPage, keyword, setNextPageData);
+  };
 
   const goToPrevPage = () => {
-    setCurrentPage(prevPage => (prevPage > 0 ? prevPage - 1 : 0));
+    const newPage = currentPage > 0 ? currentPage - 1 : 0;
+    setCurrentPage(newPage);
   };
   
   return (
@@ -161,7 +162,7 @@ function Home({ initialPoetryData }) {
         <a href="/caocaoshiji" onClick={(e) => handleCategoryChange('caocaoshiji', e)}>曹操诗集</a>
       </nav>
       
-<main id="poetry-content">
+      <main id="poetry-content">
         {Array.isArray(poetryData) && poetryData.map((poem, index) => (
           <div key={index} className="poem">
             <Poem
